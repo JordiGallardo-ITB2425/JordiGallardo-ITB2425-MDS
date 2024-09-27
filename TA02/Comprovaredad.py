ffed02f0642cc8a86e3123ebcfe60471d5835022
jordi.gallardo.7e8 <jordi.gallardo.7e8@itb.cat>
2024-09-27 09:55:15 +0200

    Subiendo Archivos

---
+++ TA02/Comprovaredad.py
@@ -0,0 +1,31 @@
+# Jordi Gallardo - 1ASIXcD
+
+# Solicitar la edad del usuario
+edat = int(input("¿Cuántos años tienes? "))
+
+# Comprobar si la persona es mayor o menor de edad
+while edat < 0:
+    print("Es imposible que aún no hayas nacido.")
+    print("Pon una edad válida.\n")
+    edat = int(input("¿Cuántos años tienes? "))
+
+while edat > 130:
+    print("No puedes ser tan viejo.")
+    print("Pon una edad válida.\n")
+    edat = int(input("¿Cuántos años tienes? "))
+
+# Categorización de la edad
+if edat <= 12:
+    print("Eres un niño.\nDisfruta de tu infancia.")
+elif edat <= 17:
+    print("Eres un adolescente.\nSal de fiesta y diviertete.")
+elif edat <= 65:
+    print("Eres un adulto.\n Trabaja para vivir y no vivas para trabajar.")
+else:
+    print("Eres una persona mayor.\nAprobecha la poca vida que te queda.")
+
+# Mensaje personalizado según si es mayor o menor de edad
+if edat >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.\nQue tengas un buen día y aprovecha tu vida.")
