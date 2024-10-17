bb963d7f88db10854f67410e28841def5e6f79ca
Jordi <jordi.gallardo.7e8@gmail.com>
2024-10-17 10:40:03 +0200

    PY modificado con color

--- TA03/MostrarPantalla.py
+++ TA03/MostrarPantalla.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+from colorama import init, Fore, Style
+init(autoreset=True)
 
 # Cargar el archivo XML
 tree = ET.parse('SolicitudAsistenciaTécnica(respostes).xml')
@@ -6,14 +8,6 @@ root = tree.getroot()
 
 # Iterar sobre las SolicitudAsistenciaTécnica(respostes) y extraer la información
 for incidencia in root.findall('incidencia'):
-    # Extraer información de la persona
-    persona = incidencia.find('persona')
-    nombre = persona.find('nombreapellidos').text.strip()
-    email = persona.find('email').text.strip()
-    
-    # Extraer información de la fecha
-    marcatiempo = incidencia.find('fecha/marcatiempo').text.strip()
-    fecha_incidencia = incidencia.find('fecha/fechaincidencia').text.strip()
     
     # Extraer información de la incidencia
     info = incidencia.find('infoincidencia')
@@ -25,14 +19,10 @@ for incidencia in root.findall('incidencia'):
     acciones_previas = info.find('accionesprevias').text.strip()
 
     # Imprimir la información de la incidencia
-    print('--- Incidencia ---')
-    print(f'Nombre: {nombre}')
-    print(f'Email: {email}')
-    print(f'Marca de tiempo: {marcatiempo}')
-    print(f'Fecha de incidencia: {fecha_incidencia}')
-    print(f'Tipo de incidencia: {tipo_incidencia}')
+    print(f'{Fore.RED}--- Incidencia ---')
+    print(f'Nivel de urgencia: {urgencia}')
     print(f'Departamento: {departamento}')
     print(f'Equipo afectado: {equipo_afectado}')
     print(f'Descripción del problema: {descripcion}')
-    print(f'Nivel de urgencia: {urgencia}')
     print(f'Acciones previas: {acciones_previas}')
+    print()
